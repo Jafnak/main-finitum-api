f19b7f8335fb0dd944fccf957297c70487e2e6ac
Jafnak <jafnakusman@gmail.com>
2025-02-28 16:09:59 +0530

    chore: Implemented Sign In & Sign Up using JWT authentication

--- app.js
+++ app.js
@@ -16,6 +16,38 @@ const generateHashedPassword = async (password) => {
     return bcrypt.hash(password, salt)
 }
 
+
+app.post("/login",(req,res)=>{
+    let input = req.body
+    userModel.find({"emailid":req.body.emailid}).then(
+    (response)=>{
+        if(response.length > 0){
+            let dbPassword = response[0].password
+            console.log(dbPassword)
+            bcrypt.compare(input.password,dbPassword,(error,isMatch)=>{ //input pswd and hashed pswd is  compared
+                if (isMatch) {
+                    //if login success generate token
+                    jwt.sign({emailid:input.emailid},"finitum-app",{expiresIn:"1d"},
+                        (error,token)=>{
+                            if (error) {
+                                res.json({"status":"unable to create token"})
+                            } else {
+                                res.json({"status":"success","userId":response[0]._id,"token":token})
+                            }
+                        }
+                    )
+                } else {
+                    res.json({"status":"incorrect"})
+                }
+            })
+            
+        } else {
+            res.json({"status":"user not found"})
+        }
+    }
+    ).catch()
+  
+})
 //---------------------USER SIGNUP----------------------------------------------------------------
 
 app.post("/usersignup", async (req, res) => {
