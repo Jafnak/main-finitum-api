b7e7201fdd7e2133763be1b952320f2d15c3fd77
Jafnak <jafnakusman@gmail.com>
2025-03-07 16:45:07 +0530

    feat:add mongodb,signup,dotenv,nodemon

---
+++ models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose")
+const schema = mongoose.Schema(
+    {
+        "name":{type:String,required:true},
+        "emailid":{type:String,required:true},
+        "age":{type:String,required:true},
+        "password":{type:String,required:true},
+        "confirmpass":{type:String,required:true},
+        "place":{type:String,required:true},
+        "interests": { type: String, enum: ['Study Group', 'Gaming', 'Health & Fitness'] }
+
+    }
+)
+let userModel = mongoose.model("users",schema)
+module.exports={userModel}
